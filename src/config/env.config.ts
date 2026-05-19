33166a3c0ba7b08fb820abcbbba390ae8811bd7e
Aarif <adebayoabdulazeez195@gmail.com>
2026-05-19 11:38:29 +0100

    add paystack configurations to environment variables and config loader

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -3,4 +3,9 @@ export default () => ({
   database: {
     uri: process.env.MONGODB_URI || "",
   },
+  paystack: {
+    secretKey: process.env.PAYSTACK_SECRET_KEY,
+    publicKey: process.env.PAYSTACK_PUBLIC_KEY,
+    callbackUrl: process.env.FRONTEND_PAYMENT_CALLBACK_URL,
+  },
 });
